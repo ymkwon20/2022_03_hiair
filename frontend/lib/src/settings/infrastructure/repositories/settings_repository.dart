ad1bb6cc90ffdecaf286dc5e2a3a2d207b5de5d1
ymkwon20 <ymkwon20@hitecis.co.kr>
2022-03-21 09:29:26 +0900

    Refactor(presentation): edit variable to const

--- frontend/lib/src/settings/infrastructure/repositories/settings_repository.dart
+++ frontend/lib/src/settings/infrastructure/repositories/settings_repository.dart
@@ -3,14 +3,9 @@ import 'package:dartz/dartz.dart';
 import 'package:frontend/src/core/failures/failure.dart';
 import 'package:frontend/src/settings/domain/entities/settings_info.dart';
 import 'package:frontend/src/settings/domain/repositories/i_settings_repository.dart';
-import 'package:frontend/src/settings/infrastructure/datasources/settings_service.dart';
 
 class SettingsRepository implements ISettingsRepository {
-  final SettingsService _service;
-
-  const SettingsRepository({
-    required SettingsService service,
-  }) : _service = service;
+  const SettingsRepository();
 
   @override
   Future<Either<Failure, SettingsInfo>> loadSettings() {
